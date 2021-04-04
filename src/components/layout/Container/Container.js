4118eb7f3ab84a46f1abb456a558fde415bc3053
Meral <meral.hadzhilar@heartgenetics.com>
2021-04-04 12:48:06 +0100

    Changed the implementation and added the spinner

--- src/components/layout/Container/Container.js
+++ src/components/layout/Container/Container.js
@@ -1,25 +1,29 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {useSelector} from "react-redux";
-import {
-    selectFetchStatus,
-    selectOrganizations,
-    selectTotalOrganizations
-} from "../../../features/organizations/organizationsSlice";
+import {selectFetchStatus, selectOrganisations} from "../../../features/organisations/organisationsSlice";
 import {SearchResults} from "../../SearchResults/SearchResults";
+import {Spinner} from "../../Spinner/Spinner";
 
 export const Container = () => {
     
-    const result = useSelector(state => selectOrganizations(state));
+    const result = useSelector(state => selectOrganisations(state));
+    const fetchStatus = useSelector(state => selectFetchStatus(state));
+    let content;
+    
+    if (Object.entries(result).length === 0 && fetchStatus === "loading") {
+        content = <Spinner size={100}/>
+    } else if (Object.entries(result).length > 0) {
+        content = <SearchResults result={result}/>;
+    } else if (Object.entries(result).length !== 0 && fetchStatus === "error") {
+        content = <span>Ups! Something went wrong.</span>;
+    } else {
+        content = null;
+    }
+    
     
     return (
         <div>
-            {/*Check if there is anything to show*/}
-            {
-                Object.entries(result).length !== 0
-                ? <SearchResults result={result}/>
-                : null
-            }
+            {content}
         </div>
-
     );
 };
