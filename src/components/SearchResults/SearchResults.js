45497d6201c35aca94d5b4b525d94dec9d522db5
Meral <meral.hadzhilar@heartgenetics.com>
2021-04-04 12:44:00 +0100

    Added the spinner, changed the implementation of the component

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -3,31 +3,35 @@ import {useSelector} from "react-redux";
 import {
     selectCurrentPage,
     selectFetchStatus,
-    selectTotalOrganizations
-} from "../../features/organizations/organizationsSlice";
-import {OrganizationsList} from "../OrganizationsList/OrganizationsList";
+    selectTotalOrganisations, selectTotalPages
+} from "../../features/organisations/organisationsSlice";
+import {OrganisationsList} from "../OrganisationsList/OrganisationsList";
 import {LoadMore} from "../LoadMore/LoadMore";
+import {Spinner} from "../Spinner/Spinner";
 
 export const SearchResults = ({result}) => {
     
-    const totalOrganizations = useSelector(state => selectTotalOrganizations(state));
+    const totalOrganisations = useSelector(state => selectTotalOrganisations(state));
     const fetchStatus = useSelector(state => selectFetchStatus(state));
     const currPage = useSelector(state => selectCurrentPage(state));
+    const totalPages = useSelector(state => selectTotalPages(state));
     
     return (
         <>
-            {
-                currPage === 1 && fetchStatus === "loading"
-                ? <span>Loading...</span>
-                :
-                    <>
-                        <h2>{`Organisations matching ´${result.queryMeta}´(${totalOrganizations})`}</h2>
-                        <div>
-                            <OrganizationsList/>
-                            <LoadMore meta={result.meta} query={result.queryMeta}/>
-                        </div>
-                    </>
-            }
+            <h2>{`Organisations matching ´${result.queryMeta}´(${totalOrganisations})`}</h2>
+            <div >
+                <OrganisationsList/>
+                {
+                    currPage >= 2 && fetchStatus === "loading"
+                        ? <Spinner size={100}/>
+                        : null
+                }
+                {
+                    totalOrganisations > 0 && currPage <= totalPages
+                        ? <LoadMore meta={result.meta} query={result.queryMeta}/>
+                        : null
+                }
+            </div>
         </>
     );
 };
