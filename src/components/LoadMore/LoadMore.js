1924fa78f00728f2467b891eb18b16d2735259a6
Meral <meral.hadzhilar@heartgenetics.com>
2021-04-04 12:44:12 +0100

    Typo

--- src/components/LoadMore/LoadMore.js
+++ src/components/LoadMore/LoadMore.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {useDispatch} from "react-redux";
-import {fetchOrganizations} from "../../features/organizations/organizationsSlice";
+import {fetchOrganisations} from "../../features/organisations/organisationsSlice";
 
 export const LoadMore = ({meta,query}) => {
     const dispatch = useDispatch();
     const {currentPage} = meta.pagination;
     const handleClick = () => {
-        dispatch(fetchOrganizations({query, perPage:6, currentPage: +currentPage + 1 }))
+        dispatch(fetchOrganisations({query, perPage: 6, currentPage: +currentPage + 1 }))
     }
     
     return (
