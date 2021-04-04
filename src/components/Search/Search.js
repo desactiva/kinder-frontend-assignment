179de32179c80d99d3ef56de2c0d11ca3e1bc212
Meral <meral.hadzhilar@heartgenetics.com>
2021-04-04 12:44:49 +0100

    Added memo to the component to reduce unnecessary rendering

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 import {useDispatch} from "react-redux";
-import {fetchOrganizations, resetState} from "../../features/organizations/organizationsSlice";
+import {fetchOrganisations, resetState} from "../../features/organisations/organisationsSlice";
 
-export const Search = () => {
+export const Search = React.memo(() => {
     const [query, setQuery] = React.useState("");
     const [perPage, setPerPage] = useState("6");
     const [currentPage, setCurrentPage] = useState("1");
@@ -10,7 +10,10 @@ export const Search = () => {
     
     const handleSubmit = (e) => {
         e.preventDefault();
-        dispatch(fetchOrganizations({query, perPage, currentPage}));
+        dispatch(resetState());
+        if (query.length > 0) {
+            dispatch(fetchOrganisations({query, perPage, currentPage}));
+        }
     }
     
     const clearSearch = (e) => {
@@ -21,7 +24,7 @@ export const Search = () => {
     
     return (
         <form onSubmit={handleSubmit}>
-            <label htmlFor="query">Search effective charitable organizations</label>
+            <label htmlFor="query">Search effective charitable organisations</label>
             <input
                 type="text"
                 name="query"
@@ -29,7 +32,7 @@ export const Search = () => {
                 onChange={(e) => setQuery(e.target.value)}
             />
             <button type="button" onClick={clearSearch}>Clear</button>
-            <button type="submit" >Search</button>
+            <button type="submit">Search</button>
         </form>
     );
-};
+});
