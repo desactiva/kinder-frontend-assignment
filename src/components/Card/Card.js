7d35e6a88ea9afa9ed3da8188f5079d97ccdac82
Meral <meral.hadzhilar@heartgenetics.com>
2021-04-04 15:56:54 +0100

    Finished the implementation of the Card component
    Added the rest of the content of the task

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,26 +1,39 @@
 import React from 'react';
 import "./Card.scss"
+import {FaSeedling} from "react-icons/fa"
 
 export const Card = ({data}) => {
     const background = data.images.data[0].files.data[0].url;
     const logo = data.logo?.data?.files?.data[0]?.url;
-    
     const categories = data.categories.data.map(c => c.name);
-    const {name, tagline, hasPassedPreliminary, publishedAt} = data;
-    // categories.data[0].name
-    
-    console.log(data, publishedAt);
+    const {
+        name,
+        tagline,
+        hasPassedPreliminary,
+        publishedAt,
+        website
+    } = data;
     
     return (
         <div className="card">
             
-            <div className="images-container">
-                <div className="logo-container">
-                    {logo ? <img src={logo} alt="organisation logo"/> : null}
-                </div>
-                <div className="background" style={{backgroundImage: `url("${background}")`}}
-                     alt="image of organisation"/>
-            </div>
+            {
+                hasPassedPreliminary && publishedAt !== null
+                    ?
+                    <a href={website} target="_blank">
+                        <div className="images-container">
+                            <div className="logo-container">
+                                {logo ? <img src={logo} alt="organisation logo"/> : null}
+                            </div>
+                            <div
+                                className="background"
+                                style={{backgroundImage: `url("${background}")`}}
+                                alt="image of organisation"/>
+                        </div>
+                    </a>
+                    : null
+            }
+            
             
             <div className="container">
                 <span>Organisation</span>
@@ -28,6 +41,18 @@ export const Card = ({data}) => {
                 <p className="tagline">{tagline}</p>
                 <p className="categories">{categories.join(' . ')}</p>
             </div>
+            
+            
+            {
+                hasPassedPreliminary && publishedAt !== null
+                    ? null
+                    :
+                    <div className="not-approved">
+                        <h5><span><FaSeedling height={10}/></span><b>Membership pending</b></h5>
+                        <p>This organisation has no published profile</p>
+                    </div>
+            }
+            
             
             <div className="state-container">
                 <div className={
